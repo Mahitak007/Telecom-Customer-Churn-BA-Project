06d1b701150f0da5b32b3ec45e8a1c376d7f2b0e
Mahitak007 <mahitak.global@gmail.com>
2026-01-18 17:54:44 -0500

    08_demographics.sql

--- sql/08_demographics.sql
+++ sql/08_demographics.sql
@@ -27,3 +27,13 @@ SELECT
     END AS Partner_Status,
     COUNT(*) AS Total_Customers,
     CAST(
+        SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)
+        AS DECIMAL(5,2)
+    ) AS Churn_Rate_Percentage
+FROM TelcoCustomerChurn
+GROUP BY
+    CASE
+        WHEN Partner = 'Yes' THEN 'Has Partner'
+        ELSE 'No Partner'
+    END;
+
